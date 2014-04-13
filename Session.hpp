ba669e276e3a1638fc61e72837f0ef5ca9a848f1
Gene Hightower <gene@digilicious.com>
2014-04-12 17:47:27 -0700

    skip black.uribl.com check if HELO/EHLO identity matches fcrdns

--- Session.hpp
+++ Session.hpp
@@ -512,11 +512,16 @@ inline bool Session::verify_recipient(Mailbox const& recipient)
 
 inline bool Session::verify_sender(Mailbox const& sender)
 {
-  DNS::Resolver res;
-  if (DNS::has_record<DNS::RR_type::A>(res, sender.domain() + ".black.uribl.com")) {
-    out() << "421 blocked by black.uribl.com\r\n" << std::flush;
-    LOG(ERROR) << sender << " blocked by black.uribl.com";
-    std::exit(EXIT_SUCCESS);
+  // If the reverse path domain matches the Forward-confirmed reverse
+  // DNS of the sending IP address, we skip the uribl check.
+  if (!Domain::match(sender.domain(), fcrdns_)) {
+    DNS::Resolver res;
+    if (DNS::has_record<DNS::RR_type::A>(res, sender.domain() +
+                                                  ".black.uribl.com")) {
+      out() << "421 blocked by black.uribl.com\r\n" << std::flush;
+      LOG(ERROR) << sender << " blocked by black.uribl.com";
+      std::exit(EXIT_SUCCESS);
+    }
   }
 
   return reverse_path_verified_ = true;
