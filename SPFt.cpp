cbcb60913c690da23e9a3f700fe19cddcfe7b5cd
Gene Hightower <gene@digilicious.com>
2014-04-13 08:44:02 -0700

    validate comment and spf strings

--- SPFt.cpp
+++ SPFt.cpp
@@ -35,18 +35,33 @@ int main(int argc, char const* argv[])
   req.set_env_from("postmaster@digilicious.com");
   SPF::Response res(req);
   CHECK_EQ(res.result(), SPF::Result::PASS);
-  std::cout << (res.header_comment() ? res.header_comment() : "") << std::endl;
-  std::cout << (res.received_spf() ? res.received_spf() : "") << std::endl;
-
-  std::cout << std::endl;
-
+  CHECK(0 == strcmp(res.header_comment(), "Example.com: domain of "
+                                          "digilicious.com designates "
+                                          "108.83.36.113 as permitted sender"));
+  CHECK(0 == strcmp(res.received_spf(), "Received-SPF: pass (Example.com: "
+                                        "domain of digilicious.com designates "
+                                        "108.83.36.113 as permitted sender) "
+                                        "client-ip=108.83.36.113; "
+                                        "envelope-from=postmaster@digilicious."
+                                        "com; helo=digilicious.com;"));
   SPF::Request req2(srv);
   req2.set_ipv4_str("10.1.1.1");
   req2.set_helo_dom("digilicious.com");
   req2.set_env_from("postmaster@digilicious.com");
   SPF::Response res2(req2);
   CHECK_EQ(res2.result(), SPF::Result::FAIL);
-  std::cout << (res2.header_comment() ? res2.header_comment() : "") << std::endl;
-  std::cout << (res2.received_spf() ? res2.received_spf() : "") << std::endl;
-  std::cout << (res2.smtp_comment() ? res2.smtp_comment() : "") << std::endl;
+  CHECK(0 == strcmp(res2.header_comment(), "Example.com: domain of "
+                                           "digilicious.com does not designate "
+                                           "10.1.1.1 as permitted sender"));
+  CHECK(0 == strcmp(res2.received_spf(), "Received-SPF: fail (Example.com: "
+                                         "domain of digilicious.com does not "
+                                         "designate 10.1.1.1 as permitted "
+                                         "sender) client-ip=10.1.1.1; "
+                                         "envelope-from=postmaster@digilicious."
+                                         "com; helo=digilicious.com;"));
+  CHECK(0 == strcmp(res2.smtp_comment(), "Please see "
+                                         "http://www.openspf.org/"
+                                         "Why?id=postmaster%40digilicious.com&"
+                                         "ip=10.1.1.1&receiver=Example.com : "
+                                         "Reason: mechanism"));
 }
