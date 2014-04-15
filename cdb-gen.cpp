315b27783b2f454087b578b2ca0e0879e551ba4b
Gene Hightower <gene@digilicious.com>
2014-04-15 14:28:18 -0700

    extra semi

--- cdb-gen.cpp
+++ cdb-gen.cpp
@@ -6,7 +6,6 @@ int main(int argc, char const* argv[])
   std::string line;
   while (std::getline(std::cin, line)) {
     std::cout << "+" << line.length() << ",1:" << line << "->1" << std::endl;
-    ;
   }
   std::cout << std::endl;
 }
