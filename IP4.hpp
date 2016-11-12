fe059ab5bdf0967c3be489e87bf834feae1f7b0c
Gene Hightower <gene@digilicious.com>
2016-11-12 10:58:39 -0800

    factor our some common code

--- IP4.hpp
+++ IP4.hpp
@@ -27,13 +27,20 @@
 
 namespace IP4 {
 
+inline boost::xpressive::cregex single_octet()
+{
+  using namespace boost::xpressive;
+
+  return (as_xpr('2') >> '5' >> range('0', '5'))
+         | ('2' >> range('0', '4') >> _d)
+         | (range('0', '1') >> repeat<1, 2>(_d)) | repeat<1, 2>(_d);
+}
+
 inline bool is_address(std::experimental::string_view addr)
 {
   using namespace boost::xpressive;
 
-  cregex octet = (as_xpr('2') >> '5' >> range('0', '5'))
-                 | ('2' >> range('0', '4') >> _d)
-                 | (range('0', '1') >> repeat<1, 2>(_d)) | repeat<1, 2>(_d);
+  auto octet = single_octet();
   cregex re = octet >> '.' >> octet >> '.' >> octet >> '.' >> octet;
   cmatch matches;
   return regex_match(addr.begin(), addr.end(), matches, re);
@@ -43,9 +50,7 @@ inline bool is_bracket_address(std::experimental::string_view addr)
 {
   using namespace boost::xpressive;
 
-  cregex octet = (as_xpr('2') >> '5' >> range('0', '5'))
-                 | ('2' >> range('0', '4') >> _d)
-                 | (range('0', '1') >> repeat<1, 2>(_d)) | repeat<1, 2>(_d);
+  auto octet = single_octet();
   cregex re
       = '[' >> octet >> '.' >> octet >> '.' >> octet >> '.' >> octet >> ']';
   cmatch matches;
@@ -56,14 +61,12 @@ inline std::string reverse(std::experimental::string_view addr)
 {
   using namespace boost::xpressive;
 
-  cregex octet = (as_xpr('2') >> '5' >> range('0', '5'))
-                 | ('2' >> range('0', '4') >> _d)
-                 | (range('0', '1') >> repeat<1, 2>(_d)) | repeat<1, 2>(_d);
+  auto octet = single_octet();
   cregex re = (s1 = octet) >> '.' >> (s2 = octet) >> '.' >> (s3 = octet) >> '.'
               >> (s4 = octet);
   cmatch matches;
   CHECK(regex_match(addr.begin(), addr.end(), matches, re))
-      << "reverse_ip4 called with bad dotted quad: " << addr;
+      << "IP4::reverse called with bad dotted quad: " << addr;
 
   std::ostringstream reverse;
   for (int n = 4; n > 0; --n) {
