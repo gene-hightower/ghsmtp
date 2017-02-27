0c82015745fb48a4b2231fe518b6b79033fa7254
Gene Hightower <gene@digilicious.com>
2017-02-27 15:40:54 -0800

    attempt to reconcile GNU AGPL with OpenSSL license terms

--- CDB.hpp
+++ CDB.hpp
@@ -1,6 +1,30 @@
 /*
     This file is part of ghsmtp - Gene's simple SMTP server.
-    Copyright (C) 2014  Gene Hightower <gene@digilicious.com>
+    Copyright © 2013-2017 Gene Hightower <gene@digilicious.com>
+
+    This program is free software: you can redistribute it and/or
+    modify it under the terms of the GNU Affero General Public License
+    as published by the Free Software Foundation, version 3.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public
+    License along with this program.  See the file COPYING.  If not,
+    see <http://www.gnu.org/licenses/>.
+
+    Additional permission under GNU AGPL version 3 section 7
+
+    If you modify this program, or any covered work, by linking or
+    combining it with the OpenSSL project's OpenSSL library (or a
+    modified version of that library), containing parts covered by the
+    terms of the OpenSSL or SSLeay licenses, I, Gene Hightower grant
+    you additional permission to convey the resulting work.
+    Corresponding Source for a non-source form of such a combination
+    shall include the source code for the parts of OpenSSL used as
+    well as that of the covered work.
 */
 
 #ifndef CDB_DOT_HPP
--- p0f.cpp
+++ p0f.cpp
@@ -1,11 +1,10 @@
 /*
     This file is part of ghsmtp - Gene's simple SMTP server.
-    Copyright © 2016 Gene Hightower <ghsmtp@digilicious.com>
+    Copyright © 2013-2017 Gene Hightower <gene@digilicious.com>
 
     This program is free software: you can redistribute it and/or
     modify it under the terms of the GNU Affero General Public License
-    as published by the Free Software Foundation, version 3 of the
-    License.
+    as published by the Free Software Foundation, version 3.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,9 +12,19 @@
     Affero General Public License for more details.
 
     You should have received a copy of the GNU Affero General Public
-    License along with this program.  If not, see
-    <http://www.gnu.org/licenses/>.
+    License along with this program.  See the file COPYING.  If not,
+    see <http://www.gnu.org/licenses/>.
 
+    Additional permission under GNU AGPL version 3 section 7
+
+    If you modify this program, or any covered work, by linking or
+    combining it with the OpenSSL project's OpenSSL library (or a
+    modified version of that library), containing parts covered by the
+    terms of the OpenSSL or SSLeay licenses, I, Gene Hightower grant
+    you additional permission to convey the resulting work.
+    Corresponding Source for a non-source form of such a combination
+    shall include the source code for the parts of OpenSSL used as
+    well as that of the covered work.
 */
 
 #include <cstddef>
