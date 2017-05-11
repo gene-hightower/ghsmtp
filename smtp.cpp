7e371cbe67ddeae2730df2782715af126960940b
Gene Hightower <gene@digilicious.com>
2017-05-11 15:23:55 -0700

    might work

--- smtp.cpp
+++ smtp.cpp
@@ -4,8 +4,6 @@
 #include <tao/pegtl/contrib/abnf.hpp>
 #include <tao/pegtl/contrib/alphabet.hpp>
 
-#include <tao/pegtl/tracer.hpp>
-
 using namespace tao::pegtl;
 using namespace tao::pegtl::abnf;
 using namespace tao::pegtl::alphabet;
@@ -14,7 +12,10 @@ namespace smtp {
 
 struct Ctx {
   Session session;
+
   Message msg;
+  size_t msg_bytes{0};
+
   std::string mb_loc;
   std::string mb_dom;
 
@@ -63,10 +64,14 @@ struct UTF8_4 : sor<seq<one<0xF0>, range<0x90, 0xBF>, rep<2, UTF8_tail>>,
 struct UTF8_non_ascii : sor<UTF8_2, UTF8_3, UTF8_4> {
 };
 
+using dot = one<'.'>;
+using colon = one<':'>;
+using dash = one<'-'>;
+
 struct U_let_dig : sor<ALPHA, DIGIT, UTF8_non_ascii> {
 };
 
-struct U_ldh_str : plus<sor<ALPHA, DIGIT, UTF8_non_ascii, one<'-'>>> {
+struct U_ldh_str : plus<sor<ALPHA, DIGIT, UTF8_non_ascii, dash>> {
   // verify last char is a U_Let_dig
 };
 
@@ -76,7 +81,7 @@ struct U_label : seq<U_let_dig, opt<U_ldh_str>> {
 struct Let_dig : sor<ALPHA, DIGIT> {
 };
 
-struct Ldh_str : plus<sor<ALPHA, DIGIT, one<'-'>>> {
+struct Ldh_str : plus<sor<ALPHA, DIGIT, dash>> {
   // verify last char is a U_Let_dig
 };
 
@@ -86,11 +91,8 @@ struct label : seq<Let_dig, opt<Ldh_str>> {
 struct sub_domain : sor<label, U_label> {
 };
 
-struct Domain : list<sub_domain, one<'.'>> {
+struct Domain : list<sub_domain, dot> {
 };
-
-using dot = one<'.'>;
-using colon = one<':'>;
 
 // clang-format off
 struct dec_octet : sor<one<'0'>,
@@ -185,13 +187,13 @@ struct atext : sor<ALPHA, DIGIT,
 struct Atom : plus<atext> {
 };
 
-struct Dot_string : list<Atom, one<'.'>> {
+struct Dot_string : list<Atom, dot> {
 };
 
-struct Local_part : sor<Quoted_string, Dot_string> {
+struct Local_part : sor<Dot_string, Quoted_string> {
 };
 
-struct Non_local_part : sor<address_literal, Domain> {
+struct Non_local_part : sor<Domain, address_literal> {
 };
 
 struct Mailbox : seq<Local_part, one<'@'>, Non_local_part> {
@@ -212,8 +214,7 @@ struct magic_postmaster : TAOCPP_PEGTL_STRING("<Postmaster>") {
 struct Forward_path : sor<Path, magic_postmaster> {
 };
 
-struct esmtp_keyword
-    : seq<sor<ALPHA, DIGIT>, star<sor<ALPHA, DIGIT, one<'-'>>>> {
+struct esmtp_keyword : seq<sor<ALPHA, DIGIT>, star<sor<ALPHA, DIGIT, dash>>> {
 };
 
 struct esmtp_value : plus<sor<range<33, 60>, range<62, 126>, UTF8_non_ascii>> {
@@ -252,6 +253,24 @@ struct rcpt_to : seq<TAOCPP_PEGTL_ISTRING("RCPT TO:"),
 struct data : seq<TAOCPP_PEGTL_ISTRING("DATA"), CRLF> {
 };
 
+struct data_end : seq<dot, CRLF> {
+};
+
+struct data_blank : CRLF {
+};
+
+struct data_dot : seq<one<'.'>, plus<not_one<'\r', '\n'>>, CRLF> {
+};
+
+struct data_plain : seq<not_one<'.'>, star<not_one<'\r', '\n'>>, CRLF> {
+};
+
+struct data_line : sor<data_blank, data_dot, data_plain> {
+};
+
+struct data_grammar : seq<star<seq<data_line, discard>>, data_end> {
+};
+
 struct rset : seq<TAOCPP_PEGTL_ISTRING("RSET"), CRLF> {
 };
 
@@ -270,27 +289,31 @@ struct starttls : seq<TAOCPP_PEGTL_ISTRING("STARTTLS"), CRLF> {
 struct quit : seq<TAOCPP_PEGTL_ISTRING("QUIT"), CRLF> {
 };
 
-struct any_cmd : sor<helo,
-                     ehlo,
-                     mail_from,
-                     rcpt_to,
-                     data,
-                     rset,
-                     noop,
-                     vrfy,
-                     help,
-                     starttls,
-                     quit> {
+// commands in size order
+
+struct any_cmd : seq<sor<data,
+                         rset,
+                         noop,
+                         vrfy,
+                         help,
+                         quit,
+                         helo,
+                         ehlo,
+                         starttls,
+                         mail_from,
+                         rcpt_to>,
+                     discard> {
 };
 
-struct cmds : plus<any_cmd> {
-};
-
-struct grammar : seq<cmds, eof> {
+struct grammar : plus<any_cmd> {
 };
 
 template <typename Rule>
 struct action : nothing<Rule> {
+};
+
+template <typename Rule>
+struct data_action : nothing<Rule> {
 };
 
 template <>
@@ -355,7 +378,8 @@ struct action<any_cmd> {
   template <typename Input>
   static void apply(const Input& in, Ctx& ctx)
   {
-    LOG(INFO) << "cmd: " << in.string();
+    // just did a discard
+    // LOG(INFO) << "cmd: " << in.string();
   }
 };
 
@@ -365,8 +389,8 @@ struct action<helo> {
   static void apply(const Input& in, Ctx& ctx)
   {
     auto ln = in.string();
-    // 5 is the length of "HELO " and two more for the CRLF.
-    auto dom = ln.substr(5, ln.length() - 7);
+    // 5 is the length of "HELO "
+    auto dom = ln.substr(5, ln.length() - 5);
     ctx.session.helo(dom);
   }
 };
@@ -377,16 +401,15 @@ struct action<ehlo> {
   static void apply(const Input& in, Ctx& ctx)
   {
     auto ln = in.string();
-    // 5 is the length of "EHLO " and two more for the CRLF.
-    auto dom = ln.substr(5, ln.length() - 7);
+    // 5 is the length of "EHLO "
+    auto dom = ln.substr(5, ln.length() - 5);
     ctx.session.ehlo(dom);
   }
 };
 
 template <>
 struct action<mail_from> {
-  template <typename Input>
-  static void apply(const Input& in, Ctx& ctx)
+  static void apply0(Ctx& ctx)
   {
     ctx.session.mail_from(::Mailbox(ctx.mb_loc, ctx.mb_dom), ctx.parameters);
 
@@ -398,8 +421,7 @@ struct action<mail_from> {
 
 template <>
 struct action<rcpt_to> {
-  template <typename Input>
-  static void apply(const Input& in, Ctx& ctx)
+  static void apply0(Ctx& ctx)
   {
     ctx.session.rcpt_to(::Mailbox(ctx.mb_loc, ctx.mb_dom), ctx.parameters);
 
@@ -410,48 +432,109 @@ struct action<rcpt_to> {
 };
 
 template <>
-struct action<rset> {
+struct data_action<data_end> {
+  static void apply0(Ctx& ctx)
+  {
+    LOG(INFO) << "data_end";
+    ctx.session.data_msg_done(ctx.msg);
+  }
+};
+
+template <>
+struct data_action<data_blank> {
+  static void apply0(Ctx& ctx)
+  {
+    LOG(INFO) << "data_blank";
+    ctx.msg.out() << '\n';
+    ctx.msg_bytes++;
+    if (ctx.msg_bytes > Config::size) {
+      LOG(WARNING) << "message size " << ctx.msg_bytes
+                   << " exceeds maximium of " << Config::size;
+    }
+  }
+};
+
+template <>
+struct data_action<data_plain> {
   template <typename Input>
   static void apply(const Input& in, Ctx& ctx)
   {
-    ctx.session.rset();
+    LOG(INFO) << "data_plain";
+    auto len = in.string().length() - 2;
+    ctx.msg.out().write(in.string().data(), len);
+    ctx.msg.out() << '\n';
+    ctx.msg_bytes += len + 1;
+    if (ctx.msg_bytes > Config::size) {
+      LOG(WARNING) << "message size " << ctx.msg_bytes
+                   << " exceeds maximium of " << Config::size;
+    }
   }
+};
+
+template <>
+struct data_action<data_dot> {
+  template <typename Input>
+  static void apply(const Input& in, Ctx& ctx)
+  {
+    LOG(INFO) << "data_dot";
+    auto len = in.string().length() - 3;
+    ctx.msg.out().write(in.string().data() + 1, len);
+    ctx.msg.out() << '\n';
+    ctx.msg_bytes += len + 1;
+    if (ctx.msg_bytes > Config::size) {
+      LOG(WARNING) << "message size " << ctx.msg_bytes
+                   << " exceeds maximium of " << Config::size;
+    }
+  }
+};
+
+template <>
+struct action<data> {
+  template <typename Input>
+  static void apply(const Input& in, Ctx& ctx)
+  {
+    LOG(INFO) << "data";
+    if (ctx.session.data_start()) {
+      ctx.session.data_msg(ctx.msg);
+      ctx.msg_bytes = 0;
+
+      istream_input<crlf_eol> data_in(ctx.session.in(), 1024, "data");
+
+      LOG(INFO) << "parsing data_grammar\n";
+      parse_nested<smtp::data_grammar, smtp::data_action>(in, data_in, ctx);
+      LOG(INFO) << "data_action parse return\n";
+    }
+  }
+};
+
+template <>
+struct action<rset> {
+  static void apply0(Ctx& ctx) { ctx.session.rset(); }
 };
 
 template <>
 struct action<noop> {
-  template <typename Input>
-  static void apply(const Input& in, Ctx& ctx)
-  {
-    ctx.session.noop();
-  }
+  static void apply0(Ctx& ctx) { ctx.session.noop(); }
 };
 
 template <>
 struct action<vrfy> {
-  template <typename Input>
-  static void apply(const Input& in, Ctx& ctx)
-  {
-    ctx.session.vrfy();
-  }
+  static void apply0(Ctx& ctx) { ctx.session.vrfy(); }
 };
 
 template <>
 struct action<help> {
-  template <typename Input>
-  static void apply(const Input& in, Ctx& ctx)
-  {
-    ctx.session.help();
-  }
+  static void apply0(Ctx& ctx) { ctx.session.help(); }
+};
+
+template <>
+struct action<starttls> {
+  static void apply0(Ctx& ctx) { ctx.session.starttls(); }
 };
 
 template <>
 struct action<quit> {
-  template <typename Input>
-  static void apply(const Input& in, Ctx& ctx)
-  {
-    ctx.session.quit();
-  }
+  static void apply0(Ctx& ctx) { ctx.session.quit(); }
 };
 }
 
@@ -469,15 +552,11 @@ int main(int argc, char const* argv[])
 
   ctx.session.in().unsetf(std::ios::skipws);
 
-  istream_input<crlf_eol> in(ctx.session.in(), 1024, "session");
+  istream_input<crlf_eol> in(ctx.session.in(), 100, "session");
 
   try {
     LOG(INFO) << "calling parse";
-#if 0
-    parse<smtp::grammar, smtp::action, tracer>(in, ctx);
-#else
     parse<smtp::grammar, smtp::action>(in, ctx);
-#endif
     LOG(INFO) << "parse return";
   }
   catch (parse_error const& e) {
