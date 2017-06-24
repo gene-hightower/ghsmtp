7f6fa872f8889a24477c6746899b37e82d0abcf3
Gene Hightower <gene@digilicious.com>
2017-06-24 12:56:49 -0700

    reinterpret_cast not allowed in constexpr

--- DKIM.hpp
+++ DKIM.hpp
@@ -11,7 +11,7 @@
 
 namespace OpenDKIM {
 
-constexpr u_char* uc(char const* cp)
+u_char* uc(char const* cp)
 {
   return reinterpret_cast<u_char*>(const_cast<char*>((cp)));
 }
--- DMARC.hpp
+++ DMARC.hpp
@@ -9,7 +9,7 @@
 
 namespace OpenDMARC {
 
-constexpr u_char* uc(char const* cp)
+u_char* uc(char const* cp)
 {
   return reinterpret_cast<u_char*>(const_cast<char*>((cp)));
 }
