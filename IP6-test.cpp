13dc11409fada42cd9608d89805ca2cfb6ee8b07
Gene Hightower <gene@digilicious.com>
2017-07-01 19:23:21 -0700

    some IPv6 tests

--- IP6-test.cpp
+++ IP6-test.cpp
@@ -10,10 +10,10 @@ int main(int argc, char const* argv[])
 {
   google::InitGoogleLogging(argv[0]);
 
-  auto as = "2001:0db8:85a3:0000:0000:8a2e:0370:7334";
+  CHECK(IP6::is_address("::1"));
+  CHECK(IP6::is_address_literal("[IPv6:::1]"));
 
-  boost::system::error_code ec;
-  boost::asio::ip::address ad = boost::asio::ip::address::from_string(as, ec);
+  CHECK(IP6::is_address("2001:0db8:85a3:0000:0000:8a2e:0370:7334"));
+  CHECK(IP6::is_address_literal("[IPv6:2001:0db8:85a3:0000:0000:8a2e:0370:7334]"));
 
-  std::cout << ad.to_string() << '\n';
 }
