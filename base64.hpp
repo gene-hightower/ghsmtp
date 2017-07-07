475a39eed8b190052dc33463b09332c6ed71824f
Gene Hightower <gene@digilicious.com>
2017-07-07 16:30:00 -0700

    format

--- base64.hpp
+++ base64.hpp
@@ -1,15 +1,18 @@
+// From https://github.com/tkislan/base64.git
+
 #ifndef BASE64_H
 #define BASE64_H
 
 #include <string>
 
 const char kBase64Alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    "abcdefghijklmnopqrstuvwxyz"
-    "0123456789+/";
+                               "abcdefghijklmnopqrstuvwxyz"
+                               "0123456789+/";
 
 class Base64 {
- public:
-  static bool Encode(const std::string &in, std::string *out) {
+public:
+  static bool Encode(const std::string& in, std::string* out)
+  {
     int i = 0, j = 0;
     size_t enc_len = 0;
     unsigned char a3[3];
@@ -52,15 +55,18 @@ class Base64 {
     return (enc_len == out->size());
   }
 
-  static bool Encode(const char *input, size_t input_length, char *out, size_t out_length) {
+  static bool
+  Encode(const char* input, size_t input_length, char* out, size_t out_length)
+  {
     int i = 0, j = 0;
-    char *out_begin = out;
+    char* out_begin = out;
     unsigned char a3[3];
     unsigned char a4[4];
 
     size_t encoded_length = EncodedLength(input_length);
 
-    if (out_length < encoded_length) return false;
+    if (out_length < encoded_length)
+      return false;
 
     while (input_length--) {
       a3[i++] = *input++;
@@ -94,7 +100,8 @@ class Base64 {
     return (out == (out_begin + encoded_length));
   }
 
-  static bool Decode(const std::string &in, std::string *out) {
+  static bool Decode(const std::string& in, std::string* out)
+  {
     int i = 0, j = 0;
     size_t dec_len = 0;
     unsigned char a3[3];
@@ -112,11 +119,11 @@ class Base64 {
 
       a4[i++] = *(input++);
       if (i == 4) {
-        for (i = 0; i <4; i++) {
+        for (i = 0; i < 4; i++) {
           a4[i] = b64_lookup(a4[i]);
         }
 
-        a4_to_a3(a3,a4);
+        a4_to_a3(a3, a4);
 
         for (i = 0; i < 3; i++) {
           (*out)[dec_len++] = a3[i];
@@ -135,7 +142,7 @@ class Base64 {
         a4[j] = b64_lookup(a4[j]);
       }
 
-      a4_to_a3(a3,a4);
+      a4_to_a3(a3, a4);
 
       for (j = 0; j < i - 1; j++) {
         (*out)[dec_len++] = a3[j];
@@ -145,15 +152,18 @@ class Base64 {
     return (dec_len == out->size());
   }
 
-  static bool Decode(const char *input, size_t input_length, char *out, size_t out_length) {
+  static bool
+  Decode(const char* input, size_t input_length, char* out, size_t out_length)
+  {
     int i = 0, j = 0;
-    char *out_begin = out;
+    char* out_begin = out;
     unsigned char a3[3];
     unsigned char a4[4];
 
     size_t decoded_length = DecodedLength(input, input_length);
 
-    if (out_length < decoded_length) return false;
+    if (out_length < decoded_length)
+      return false;
 
     while (input_length--) {
       if (*input == '=') {
@@ -162,11 +172,11 @@ class Base64 {
 
       a4[i++] = *(input++);
       if (i == 4) {
-        for (i = 0; i <4; i++) {
+        for (i = 0; i < 4; i++) {
           a4[i] = b64_lookup(a4[i]);
         }
 
-        a4_to_a3(a3,a4);
+        a4_to_a3(a3, a4);
 
         for (i = 0; i < 3; i++) {
           *out++ = a3[i];
@@ -185,7 +195,7 @@ class Base64 {
         a4[j] = b64_lookup(a4[j]);
       }
 
-      a4_to_a3(a3,a4);
+      a4_to_a3(a3, a4);
 
       for (j = 0; j < i - 1; j++) {
         *out++ = a3[j];
@@ -195,62 +205,76 @@ class Base64 {
     return (out == (out_begin + decoded_length));
   }
 
-  static int DecodedLength(const char *in, size_t in_length) {
+  static int DecodedLength(const char* in, size_t in_length)
+  {
     int numEq = 0;
 
-    const char *in_end = in + in_length;
-    while (*--in_end == '=') ++numEq;
+    const char* in_end = in + in_length;
+    while (*--in_end == '=')
+      ++numEq;
 
     return ((6 * in_length) / 8) - numEq;
   }
 
-  static int DecodedLength(const std::string &in) {
+  static int DecodedLength(const std::string& in)
+  {
     int numEq = 0;
     int n = in.size();
 
-    for (std::string::const_reverse_iterator it = in.rbegin(); *it == '='; ++it) {
+    for (std::string::const_reverse_iterator it = in.rbegin(); *it == '=';
+         ++it) {
       ++numEq;
     }
 
     return ((6 * n) / 8) - numEq;
   }
 
-  inline static int EncodedLength(size_t length) {
+  inline static int EncodedLength(size_t length)
+  {
     return (length + 2 - ((length + 2) % 3)) / 3 * 4;
   }
 
-  inline static int EncodedLength(const std::string &in) {
+  inline static int EncodedLength(const std::string& in)
+  {
     return EncodedLength(in.length());
   }
 
-  inline static void StripPadding(std::string *in) {
-    while (!in->empty() && *(in->rbegin()) == '=') in->resize(in->size() - 1);
+  inline static void StripPadding(std::string* in)
+  {
+    while (!in->empty() && *(in->rbegin()) == '=')
+      in->resize(in->size() - 1);
   }
 
- private:
-  static inline void a3_to_a4(unsigned char * a4, unsigned char * a3) {
+private:
+  static inline void a3_to_a4(unsigned char* a4, unsigned char* a3)
+  {
     a4[0] = (a3[0] & 0xfc) >> 2;
     a4[1] = ((a3[0] & 0x03) << 4) + ((a3[1] & 0xf0) >> 4);
     a4[2] = ((a3[1] & 0x0f) << 2) + ((a3[2] & 0xc0) >> 6);
     a4[3] = (a3[2] & 0x3f);
   }
 
-  static inline void a4_to_a3(unsigned char * a3, unsigned char * a4) {
+  static inline void a4_to_a3(unsigned char* a3, unsigned char* a4)
+  {
     a3[0] = (a4[0] << 2) + ((a4[1] & 0x30) >> 4);
     a3[1] = ((a4[1] & 0xf) << 4) + ((a4[2] & 0x3c) >> 2);
     a3[2] = ((a4[2] & 0x3) << 6) + a4[3];
   }
 
-  static inline unsigned char b64_lookup(unsigned char c) {
-    if(c >='A' && c <='Z') return c - 'A';
-    if(c >='a' && c <='z') return c - 71;
-    if(c >='0' && c <='9') return c + 4;
-    if(c == '+') return 62;
-    if(c == '/') return 63;
+  static inline unsigned char b64_lookup(unsigned char c)
+  {
+    if (c >= 'A' && c <= 'Z')
+      return c - 'A';
+    if (c >= 'a' && c <= 'z')
+      return c - 71;
+    if (c >= '0' && c <= '9')
+      return c + 4;
+    if (c == '+')
+      return 62;
+    if (c == '/')
+      return 63;
     return 255;
   }
 };
-
-
 
 #endif // BASE64_H
