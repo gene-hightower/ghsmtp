eaeb493d3c7bece5813383a6c5ec798a6b770d6c
Gene Hightower <gene@digilicious.com>
2017-07-12 17:58:06 -0700

    this works for GCC7

--- Pill.cpp
+++ Pill.cpp
@@ -29,27 +29,27 @@ Pill::Pill()
     do {
       x = *--osp;
       b32_str_[--resp] = b32_charset[x % 32];
-      x /= 32;
-      BOOST_FALLTHROUGH;
+      x /= 32; // falls through
+
     case 4:
       x |= (static_cast<unsigned long>(*--osp)) << 3;
       b32_str_[--resp] = b32_charset[x % 32];
       x /= 32;
       b32_str_[--resp] = b32_charset[x % 32];
-      x /= 32;
-      BOOST_FALLTHROUGH;
+      x /= 32; // falls through
+
     case 3:
       x |= (static_cast<unsigned long>(*--osp)) << 1;
       b32_str_[--resp] = b32_charset[x % 32];
-      x /= 32;
-      BOOST_FALLTHROUGH;
+      x /= 32; // falls through
+
     case 2:
       x |= (static_cast<unsigned long>(*--osp)) << 4;
       b32_str_[--resp] = b32_charset[x % 32];
       x /= 32;
       b32_str_[--resp] = b32_charset[x % 32];
-      x /= 32;
-      BOOST_FALLTHROUGH;
+      x /= 32; // falls through
+
     case 1:
       x |= (static_cast<unsigned long>(*--osp)) << 2;
       b32_str_[--resp] = b32_charset[x % 32];
