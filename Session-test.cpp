b1a8c4d023aa8bd02df853417a09aef123154060
Gene Hightower <gene@digilicious.com>
2017-07-13 20:17:57 -0700

    read_hook

--- Session-test.cpp
+++ Session-test.cpp
@@ -15,8 +15,7 @@ struct Session_test {
     PCHECK(fd_null >= 0) << " can't open /dev/null";
 
     auto read_hook = []() { std::cout << "Session-test read_hook\n"; };
-
-    Session sess(STDIN_FILENO, fd_null, read_hook, "example.com");
+    Session sess(read_hook, STDIN_FILENO, fd_null, "example.com");
 
     LOG(ERROR) << "Expect: 3 invalid sender domains:";
     CHECK(!sess.verify_sender_domain_(Domain("com")));
