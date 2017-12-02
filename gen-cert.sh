d38222a402c82f8f9a04c3952fb68c58d667d291
Gene Hightower <gene@digilicious.com>
2017-12-02 10:19:23 -0800

    make new cert

---
+++ gen-cert.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+
+# openssl req -out smtp.csr -new -newkey rsa:4096 -nodes -keyout smtp.key
+
+mv smtp.pem smtp.pem-`date +%Y-%m-%d-%h:%m:%S.%N`
+
+CN=`hostname`
+
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:4096 -keyout smtp.key -out smtp.crt \
+ -subj "/C=US/ST=CA/L=Los Angeles/CN=$CN"
+
+cat smtp.key smtp.crt > smtp.pem
+
+tlsa --certificate smtp.crt $CN
