14edff3b65d9668068264eb111050c97da41adea
Gene Hightower <gene@digilicious.com>
2018-01-15 13:38:55 -0800

    shut

--- ouroboros.sh
+++ ouroboros.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 coproc MAILDIR=/tmp/Maildir GOOGLE_LOG_DIR=/tmp/Maillogs ./smtp -log_data=true
-./snd -log_data=true -pipe=true <&${COPROC[0]} >&${COPROC[1]}
+./snd -pipe=true <&${COPROC[0]} >&${COPROC[1]}
