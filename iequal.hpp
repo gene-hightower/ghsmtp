0276991e47514f83c620a0f5a6733bf6292ab66e
Gene Hightower <gene@digilicious.com>
2018-01-18 15:05:05 -0800

    new world

--- iequal.hpp
+++ iequal.hpp
@@ -6,13 +6,13 @@
 
 // Like boost, but ASCII only.  No locale required.
 
-inline bool iequal_char(char a, char b)
+auto constexpr iequal_char(char a, char b) -> bool
 {
   return std::toupper(static_cast<unsigned char>(a))
          == std::toupper(static_cast<unsigned char>(b));
 }
 
-inline bool iequal(std::string_view a, std::string_view b)
+auto constexpr iequal(std::string_view a, std::string_view b) -> bool
 {
   if (a.length() == b.length()) {
     return std::equal(b.begin(), b.end(), a.begin(), iequal_char);
