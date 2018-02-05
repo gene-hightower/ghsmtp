c99c8e285f6c9cf4c78ef609712cd487f9bd16d6
Gene Hightower <gene@digilicious.com>
2018-02-04 18:57:41 -0800

    maybe not very useful

--- cov.sh
+++ cov.sh
@@ -6,7 +6,6 @@ rm -f ${PROF} smtp.profdata
 
 PROFD=/tmp/smtp-profile
 
-rm -rf ${PROFD}
 mkdir -p ${PROFD}
 
 ASAN_OPTIONS=detect_odr_violation=0 ./snd
@@ -18,16 +17,12 @@ mv ${PROF} ${PROFD}/${PROF}.1
 ASAN_OPTIONS=detect_odr_violation=0 ./snd -huge_size
 mv ${PROF} ${PROFD}/${PROF}.2
 
-ASAN_OPTIONS=detect_odr_violation=0 ./snd -ip4
+ASAN_OPTIONS=detect_odr_violation=0 ./snd -4
 mv ${PROF} ${PROFD}/${PROF}.3
 
 ASAN_OPTIONS=detect_odr_violation=0 ./snd -use_esmtp=false
 mv ${PROF} ${PROFD}/${PROF}.4
 
 llvm-profdata merge -sparse \
-${PROFD}/${PROF}.0 \
-${PROFD}/${PROF}.1 \
-${PROFD}/${PROF}.2 \
-${PROFD}/${PROF}.3 \
-${PROFD}/${PROF}.4 \
+${PROFD}/* \
 -o smtp.profdata
