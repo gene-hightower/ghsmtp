014c9ea0448a6c93e62d93805cf0e2529dc2c668
Gene Hightower <gene@digilicious.com>
2018-03-16 20:48:21 -0700

    new wrappers for ldns, still working on this

---
+++ DNS-ldns.cpp
@@ -0,0 +1,282 @@
+#include "DNS-ldns.hpp"
+
+#include <cstring>
+#include <iomanip>
+
+#include <cstdbool> // needs to be above ldns includes
+#include <ldns/ldns.h>
+#include <ldns/packet.h>
+#include <ldns/rr.h>
+
+#include <arpa/inet.h>
+
+#include <glog/logging.h>
+
+namespace DNS {
+
+RR_A::RR_A(uint8_t const* rd, size_t sz)
+{
+  CHECK_EQ(sz, sizeof(addr_.sin_addr));
+  std::memcpy(&addr_.sin_addr, rd, sizeof(addr_.sin_addr));
+  PCHECK(inet_ntop(AF_INET, &addr_.sin_addr, str_, sizeof str_));
+}
+
+RR_AAAA::RR_AAAA(uint8_t const* rd, size_t sz)
+{
+  CHECK_EQ(sz, sizeof(addr_.sin6_addr));
+  std::memcpy(&addr_.sin6_addr, rd, sizeof(addr_.sin6_addr));
+  PCHECK(inet_ntop(AF_INET6, &addr_.sin6_addr, str_, sizeof str_));
+}
+
+std::string rr_name_str(ldns_rdf const* rdf)
+{
+  auto sz = ldns_rdf_size(rdf);
+
+  if (sz > LDNS_MAX_DOMAINLEN) {
+    LOG(WARNING) << "rdf size too large";
+    return "<too long>";
+  }
+  if (sz == 1) {
+    return "."; // root label
+  }
+
+  auto data = ldns_rdf_data(rdf);
+
+  unsigned char src_pos = 0;
+  unsigned char len = data[src_pos];
+
+  std::ostringstream str;
+  while ((len > 0) && (src_pos < ldns_rdf_size(rdf))) {
+    src_pos++;
+    for (unsigned char i = 0; i < len; ++i) {
+      unsigned char c = data[src_pos];
+      if (c == '.' || c == ';' || c == '(' || c == ')' || c == '\\') {
+        str << '\\' << c;
+      }
+      else if (!(isascii(c) && isgraph(c))) {
+        str << "0x" << std::hex << std::setfill('0') << std::setw(2)
+            << static_cast<unsigned>(c);
+      }
+      else {
+        str << c;
+      }
+      src_pos++;
+    }
+    if (src_pos < ldns_rdf_size(rdf)) {
+      str << '.';
+    }
+    len = data[src_pos];
+  }
+
+  return str.str();
+}
+
+std::string rr_str(ldns_rdf const* rdf)
+{
+  CHECK_NOTNULL(rdf);
+
+  auto data = static_cast<char const*>(rdf->_data);
+  auto udata = static_cast<unsigned char const*>(rdf->_data);
+
+  return std::string(data + 1, static_cast<std::string::size_type>(*udata));
+}
+
+Resolver::Resolver()
+{
+  auto status = ldns_resolver_new_frm_file(&res_, nullptr);
+  CHECK_EQ(status, LDNS_STATUS_OK) << "failed to initialize DNS resolver: "
+                                   << ldns_get_errorstr_by_id(status);
+}
+
+Resolver::~Resolver() { ldns_resolver_deep_free(res_); }
+
+Domain::Domain(char const* domain)
+  : str_(domain)
+  , drdfp_(CHECK_NOTNULL(ldns_dname_new_frm_str(domain)))
+{
+}
+
+Domain::~Domain() { ldns_rdf_deep_free(drdfp_); }
+
+Query::Query(Resolver const& res, RRtype type, Domain const& dom)
+{
+  ldns_status status = ldns_resolver_query_status(
+      &p_, res.res_, dom.drdfp_, static_cast<ldns_enum_rr_type>(type),
+      LDNS_RR_CLASS_IN, LDNS_RD | LDNS_AD);
+
+  if (status != LDNS_STATUS_OK) {
+    bogus_or_indeterminate_ = true;
+
+    LOG(WARNING) << "Query (" << dom.str_ << "/" << RRtype_c_str(type) << ") "
+                 << "ldns_resolver_query_status failed: "
+                 << ldns_get_errorstr_by_id(status);
+
+    // If we have only one nameserver, reset the RTT otherwise all
+    // future use of this resolver object will fail.
+
+    if (ldns_resolver_nameserver_count(res.res_) == 1) {
+      if (ldns_resolver_rtt(res.res_) == LDNS_RESOLV_RTT_INF) {
+        ldns_resolver_set_nameserver_rtt(res.res_, 0,
+                                         LDNS_RESOLV_RTT_MIN); // "reachable"
+      }
+    }
+    return;
+  }
+
+  auto const rcode = ldns_pkt_get_rcode(p_);
+
+  switch (rcode) {
+  case LDNS_RCODE_NOERROR:
+    break;
+
+  case LDNS_RCODE_NXDOMAIN:
+    nx_domain_ = true;
+    LOG(WARNING) << "NX domain (" << dom.str_ << "/" << RRtype_c_str(type)
+                 << ")";
+    return;
+
+  default:
+    bogus_or_indeterminate_ = true;
+    LOG(WARNING) << "DNS query (" << dom.str_ << "/" << RRtype_c_str(type)
+                 << ") ldns_resolver_query_status failed: rcode=" << rcode;
+    return;
+  }
+}
+
+Query::~Query()
+{
+  if (p_)
+    ldns_pkt_free(p_);
+}
+
+RR_list::RR_list(Query const& q)
+{
+  if (q.p_) {
+    // no clones, so no frees required
+    rrlst_answer_ = ldns_pkt_answer(q.p_);
+    rrlst_additional_ = ldns_pkt_additional(q.p_);
+  }
+}
+
+RR_list::~RR_list() {}
+
+RR_set RR_list::get() const
+{
+  RR_set ret;
+
+  if (rrlst_answer_) {
+    LOG(INFO) << "ldns_rr_list_rr_count(rrlst_answer_) == "
+              << ldns_rr_list_rr_count(rrlst_answer_);
+
+    ret.reserve(ldns_rr_list_rr_count(rrlst_answer_));
+
+    for (unsigned i = 0; i < ldns_rr_list_rr_count(rrlst_answer_); ++i) {
+      auto const rr = ldns_rr_list_rr(rrlst_answer_, i);
+
+      if (rr) {
+        LOG(INFO) << "ldns_rr_rd_count(rr) == " << ldns_rr_rd_count(rr);
+        LOG(INFO) << "ldns_rr_get_type(rr) == " << ldns_rr_get_type(rr);
+
+        switch (ldns_rr_get_type(rr)) {
+        case LDNS_RR_TYPE_A: {
+          CHECK_EQ(ldns_rr_rd_count(rr), 1);
+          auto const rdf = ldns_rr_rdf(rr, 0);
+          CHECK_EQ(ldns_rdf_get_type(rdf), LDNS_RDF_TYPE_A);
+          ret.emplace_back(RR_A{ldns_rdf_data(rdf), ldns_rdf_size(rdf)});
+          break;
+        }
+        case LDNS_RR_TYPE_CNAME: {
+          CHECK_EQ(ldns_rr_rd_count(rr), 1);
+          auto const rdf = ldns_rr_rdf(rr, 0);
+          CHECK_EQ(ldns_rdf_get_type(rdf), LDNS_RDF_TYPE_DNAME);
+          ret.emplace_back(RR_CNAME{rr_name_str(rdf)});
+          break;
+        }
+        case LDNS_RR_TYPE_PTR: {
+          CHECK_EQ(ldns_rr_rd_count(rr), 1);
+          auto const rdf = ldns_rr_rdf(rr, 0);
+          CHECK_EQ(ldns_rdf_get_type(rdf), LDNS_RDF_TYPE_DNAME);
+          ret.emplace_back(RR_PTR{rr_name_str(rdf)});
+          break;
+        }
+        case LDNS_RR_TYPE_MX: {
+          CHECK_EQ(ldns_rr_rd_count(rr), 2);
+          auto const rdf_0 = ldns_rr_rdf(rr, 0);
+          CHECK_EQ(ldns_rdf_get_type(rdf_0), LDNS_RDF_TYPE_INT16);
+          auto const rdf_1 = ldns_rr_rdf(rr, 1);
+          CHECK_EQ(ldns_rdf_get_type(rdf_1), LDNS_RDF_TYPE_DNAME);
+          ret.emplace_back(
+              RR_MX{rr_name_str(rdf_1), ldns_rdf2native_int16(rdf_0)});
+          break;
+        }
+        case LDNS_RR_TYPE_TXT: {
+          CHECK_EQ(ldns_rr_rd_count(rr), 1);
+          auto const rdf = ldns_rr_rdf(rr, 0);
+          CHECK_EQ(ldns_rdf_get_type(rdf), LDNS_RDF_TYPE_STR);
+          ret.emplace_back(RR_TXT{rr_str(rdf)});
+          break;
+        }
+        case LDNS_RR_TYPE_AAAA: {
+          CHECK_EQ(ldns_rr_rd_count(rr), 1);
+          auto const rdf = ldns_rr_rdf(rr, 0);
+          CHECK_EQ(ldns_rdf_get_type(rdf), LDNS_RDF_TYPE_AAAA);
+          ret.emplace_back(RR_AAAA{ldns_rdf_data(rdf), ldns_rdf_size(rdf)});
+          break;
+        }
+
+        case LDNS_RR_TYPE_TLSA:
+          LOG(WARNING) << "no code for LDNS_RR_TYPE_TLSA";
+          break;
+
+        default:
+          LOG(WARNING) << "unknown RR type == " << ldns_rr_get_type(rr);
+          break;
+        }
+      }
+    }
+  }
+
+  LOG(INFO) << "check for additional RRs";
+  if (rrlst_additional_) {
+    LOG(INFO) << "ldns_rr_list_rr_count(rrlst_additional_) == "
+              << ldns_rr_list_rr_count(rrlst_additional_);
+
+    for (unsigned i = 0; i < ldns_rr_list_rr_count(rrlst_additional_); ++i) {
+      auto const rr_additional = ldns_rr_list_rr(rrlst_additional_, i);
+
+      if (rr_additional) {
+        for (unsigned j = 0; j < ldns_rr_rd_count(rr_additional); ++j) {
+          auto const rdf = ldns_rr_rdf(rr_additional, j);
+          auto const type = ldns_rdf_get_type(rdf);
+          switch (type) {
+          case LDNS_RDF_TYPE_A: {
+            auto arec{RR_A{ldns_rdf_data(rdf), ldns_rdf_size(rdf)}};
+            LOG(INFO) << "A == " << arec.c_str();
+            break;
+          }
+          case LDNS_RDF_TYPE_AAAA: {
+            auto aaaarec{RR_AAAA{ldns_rdf_data(rdf), ldns_rdf_size(rdf)}};
+            LOG(INFO) << "AAAA == " << aaaarec.c_str();
+            break;
+          }
+          default:
+            LOG(WARNING) << "non A/AAAA for MX additional section, type="
+                         << static_cast<unsigned>(type);
+          }
+        }
+      }
+    }
+  }
+
+  return ret;
+}
+
+RR_set Resolver::get_records(RRtype typ, char const* domain)
+{
+  Domain dom(domain);
+  Query q(*this, typ, dom);
+  RR_list rrlst(q);
+  return rrlst.get();
+}
+
+} // namespace DNS
---
+++ DNS-ldns.hpp
@@ -0,0 +1,227 @@
+#ifndef DNS_LDNS_DOT_HPP
+#define DNS_LDNS_DOT_HPP
+
+#include <cstddef>
+#include <string>
+#include <variant>
+#include <vector>
+
+#include <netinet/in.h>
+
+// forward decl
+typedef struct ldns_struct_pkt ldns_pkt;
+typedef struct ldns_struct_rdf ldns_rdf;
+typedef struct ldns_struct_resolver ldns_resolver;
+typedef struct ldns_struct_rr_list ldns_rr_list;
+
+namespace DNS {
+
+enum class RRtype : uint16_t {
+  // RFC 1035 section 3.2.2 TYPE values:
+  NONE,
+  A,
+  NS,
+  MD,
+  MF,
+  CNAME,
+  SOA,
+  MB,
+  MG,
+  MR,
+  RR_NULL,
+  WKS,
+  PTR,
+  HINFO,
+  MINFO,
+  MX,
+  TXT,
+
+  // RFC 3596 section 2.1 AAAA record type
+  AAAA = 28,
+
+  // RFC 6698 section 7.1 TLSA RRtype
+  TLSA = 52,
+};
+
+auto constexpr RRtype_c_str(RRtype const& type)
+{
+  // clang-format off
+  switch (type) {
+  case RRtype::NONE:   return "NONE";
+  case RRtype::A:      return "A";
+  case RRtype::NS:     return "NS";
+  case RRtype::MD:     return "MD";
+  case RRtype::MF:     return "MF";
+  case RRtype::CNAME:  return "CNAME";
+  case RRtype::SOA:    return "SOA";
+  case RRtype::MB:     return "MB";
+  case RRtype::MG:     return "MG";
+  case RRtype::MR:     return "MR";
+  case RRtype::RR_NULL:return "RR_NULL";
+  case RRtype::WKS:    return "WKS";
+  case RRtype::PTR:    return "PTR";
+  case RRtype::HINFO:  return "HINFO";
+  case RRtype::MINFO:  return "MINFO";
+  case RRtype::MX:     return "MX";
+  case RRtype::TXT:    return "TXT";
+  case RRtype::AAAA:   return "AAAA";
+  case RRtype::TLSA:   return "TLSA";
+  }
+  return "** Unknown **";
+  // clang-format on
+}
+
+class RR_A {
+public:
+  RR_A(uint8_t const* rd, size_t sz);
+
+  sockaddr_in const& addr() const { return addr_; }
+  char const* c_str() const { return str_; }
+
+private:
+  sockaddr_in addr_;
+  char str_[INET_ADDRSTRLEN];
+};
+
+class RR_CNAME {
+public:
+  explicit RR_CNAME(std::string cname)
+    : cname_(cname)
+  {
+  }
+
+  std::string const& str() const { return cname_; }
+
+private:
+  std::string cname_;
+};
+
+// RFC 2181 section 10.2 PTR records
+class RR_PTR {
+public:
+  explicit RR_PTR(std::string ptrdname)
+    : ptrdname_(ptrdname)
+  {
+  }
+
+  std::string const& str() const { return ptrdname_; }
+
+private:
+  std::string ptrdname_;
+};
+
+class RR_MX {
+public:
+  RR_MX(std::string exchange, uint16_t preference)
+    : exchange_(exchange)
+    , preference_(preference)
+  {
+  }
+
+  std::string const& exchange() const { return exchange_; }
+  uint16_t preference() const { return preference_; }
+
+private:
+  std::string exchange_;
+  uint16_t preference_;
+};
+
+class RR_TXT {
+public:
+  explicit RR_TXT(std::string txt_data)
+    : txt_data_(txt_data)
+  {
+  }
+
+  std::string const& str() const { return txt_data_; }
+
+private:
+  std::string txt_data_;
+};
+
+class RR_AAAA {
+public:
+  RR_AAAA(uint8_t const* rd, size_t sz);
+
+  sockaddr_in6 const& addr() const { return addr_; }
+  char const* c_str() const { return str_; }
+
+private:
+  sockaddr_in6 addr_;
+  char str_[INET6_ADDRSTRLEN];
+};
+
+using RR = std::variant<RR_A, RR_CNAME, RR_PTR, RR_MX, RR_TXT, RR_AAAA>;
+
+using RR_set = std::vector<RR>;
+
+class Resolver {
+public:
+  Resolver(Resolver const&) = delete;
+  Resolver& operator=(Resolver const&) = delete;
+
+  Resolver();
+  ~Resolver();
+
+  RR_set get_records(RRtype typ, char const* domain);
+
+private:
+  ldns_resolver* res_;
+
+  friend class Query;
+};
+
+class Domain {
+public:
+  Domain(Domain const&) = delete;
+  Domain& operator=(Domain const&) = delete;
+
+  explicit Domain(char const* domain);
+  ~Domain();
+
+private:
+  std::string str_;
+  ldns_rdf* drdfp_;
+
+  friend class Query;
+};
+
+class Query {
+public:
+  Query(Query const&) = delete;
+  Query& operator=(Query const&) = delete;
+
+  Query(Resolver const& res, RRtype type, Domain const& dom);
+  ~Query();
+
+  // Pkt_rcode get_rcode() const;
+
+  bool bogus_or_indeterminate() const { return bogus_or_indeterminate_; }
+
+private:
+  ldns_pkt* p_{nullptr};
+
+  bool bogus_or_indeterminate_{false};
+  bool nx_domain_{false};
+
+  friend class RR_list;
+};
+
+class RR_list {
+public:
+  RR_list(RR_list const&) = delete;
+  RR_list& operator=(RR_list const&) = delete;
+
+  explicit RR_list(Query const& q);
+  ~RR_list();
+
+  RR_set get() const;
+
+private:
+  ldns_rr_list* rrlst_answer_{nullptr};
+  ldns_rr_list* rrlst_additional_{nullptr};
+};
+
+} // namespace DNS
+
+#endif // DNS_LDNS_DOT_HPP
