3d8625e45bc412f954db56368be63229e0d50314
Gene Hightower <gene@digilicious.com>
2018-05-16 15:29:12 -0700

    find a nameserver

--- dnsprv.cpp
+++ dnsprv.cpp
@@ -1,4 +1,6 @@
-#include <iostream>
+#include <memory>
+
+#include <experimental/random>
 
 #include "Sock.hpp"
 #include "osutil.hpp"
@@ -9,33 +11,78 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
-auto constexpr addr4 = "1.1.1.1";
+template <typename T, std::size_t N>
+constexpr std::size_t countof(T const (&)[N]) noexcept
+{
+  return N;
+}
 
-auto constexpr srv = "domain-s";
+struct nameserver {
+  char const* host;
+  char const* addr;
+  char const* port;
+};
 
-auto constexpr client_name = "digilicious.com";
-auto constexpr server_name = "1dot1dot1dot1.cloudflare-dns.com";
+constexpr nameserver nameservers[]{
+    {
+        "1dot1dot1dot1.cloudflare-dns.com",
+        "1.0.0.1",
+        "domain-s",
+    },
+    {
+        "1dot1dot1dot1.cloudflare-dns.com",
+        "1.1.1.1",
+        "domain-s",
+    },
+    {
+        "dns.quad9.net",
+        "9.9.9.10",
+        "domain-s",
+    },
+};
 
 int main(int argc, char* argv[])
 {
-  uint16_t port = osutil::get_port(srv);
+  auto sock{[&] {
+    auto tries = countof(nameservers);
+    auto ns = std::experimental::randint(
+        0, static_cast<int>(countof(nameservers) - 1));
 
-  auto const fd{socket(AF_INET, SOCK_STREAM, 0)};
-  PCHECK(fd >= 0) << "socket() failed";
+    while (tries--) {
+      // try the next one, with wrap
+      if (++ns == countof(nameservers)) {
+        ns = 0;
+      }
 
-  auto in4{sockaddr_in{}};
-  in4.sin_family = AF_INET;
-  in4.sin_port = htons(port);
-  CHECK_EQ(inet_pton(AF_INET, addr4, reinterpret_cast<void*>(&in4.sin_addr)),
-           1);
-  if (connect(fd, reinterpret_cast<const sockaddr*>(&in4), sizeof(in4))) {
-    PLOG(FATAL) << "connect failed [" << addr4 << "]:" << port;
-  }
+      auto const fd{socket(AF_INET, SOCK_STREAM, 0)};
+      PCHECK(fd >= 0) << "socket() failed";
 
-  Sock sock(fd, fd);
+      uint16_t port = osutil::get_port(nameservers[ns].port);
 
-  DNS::RR_set tlsa_rrs; // empty
+      auto in4{sockaddr_in{}};
+      in4.sin_family = AF_INET;
+      in4.sin_port = htons(port);
+      CHECK_EQ(inet_pton(AF_INET, nameservers[ns].addr,
+                         reinterpret_cast<void*>(&in4.sin_addr)),
+               1);
+      if (connect(fd, reinterpret_cast<const sockaddr*>(&in4), sizeof(in4))) {
+        PLOG(INFO) << "connect failed " << nameservers[ns].host << '['
+                   << nameservers[ns].addr << "]:" << nameservers[ns].port;
+        close(fd);
+        continue;
+      }
+      auto sock = std::make_shared<Sock>(fd, fd);
+      DNS::RR_set tlsa_rrs; // empty
+      sock->starttls_client(nullptr, nameservers[ns].host, tlsa_rrs, false);
+      if (sock->verified()) {
+        LOG(INFO) << "using " << nameservers[ns].host << '['
+                  << nameservers[ns].addr << "]:" << nameservers[ns].port;
+        return sock;
+      }
 
-  sock.starttls_client(client_name, server_name, tlsa_rrs, false);
-  CHECK(sock.verified());
+      close(fd);
+    }
+
+    LOG(FATAL) << "no nameservers left to try";
+  }()};
 }
