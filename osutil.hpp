f40c268346d2d8678a1d8902db96cb44b2b9848d
Gene Hightower <gene@digilicious.com>
2018-05-22 12:12:25 -0700

    include guards

--- osutil.hpp
+++ osutil.hpp
@@ -1,6 +1,8 @@
-#pragma once
+#ifndef OSUTIL_DOT_HPP_INCLUDED
+#define OSUTIL_DOT_HPP_INCLUDED
 
 #include <string>
+#include <vector>
 
 #include "fs.hpp"
 
@@ -12,3 +14,5 @@ uint16_t get_port(char const* const service);
 std::vector<fs::path> list_directory(fs::path const& path,
                                      std::string const& pattern);
 } // namespace osutil
+
+#endif // OSUTIL_DOT_HPP_INCLUDED
