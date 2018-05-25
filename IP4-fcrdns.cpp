b85a003296c4f32dd284f6c618b0a1f8b71a291b
Gene Hightower <gene@digilicious.com>
2018-05-25 11:00:27 -0700

    no longer

--- IP4-fcrdns.cpp
+++
@@ -1,9 +0,0 @@
-#include "IP4.hpp"
-
-#include <glog/logging.h>
-
-#include "DNS.hpp"
-
-namespace IP4 {
-
-} // namespace IP4
--- IP6-fcrdns.cpp
+++
@@ -1,9 +0,0 @@
-#include "IP6.hpp"
-
-#include <glog/logging.h>
-
-#include "DNS.hpp"
-
-namespace IP6 {
-
-} // namespace IP6
