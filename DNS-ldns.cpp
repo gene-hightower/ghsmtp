86b86344e486864559bdc9b6525d1abad826fda8
Gene Hightower <gene@digilicious.com>
2018-06-02 14:42:20 -0700

    keep it working in case we need it

--- DNS-ldns.cpp
+++ DNS-ldns.cpp
@@ -281,7 +281,7 @@ RR_set RR_list::get_records() const
         if (rr) {
           auto type = ldns_rr_get_type(rr);
           LOG(WARNING) << "additional record " << i << " type "
-                       << rr_type_c_str_(type);
+                       << RR_type_c_str(type);
         }
       }
     }
