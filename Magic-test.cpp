e217ff9afa8f279818a026e476a23713127d36c1
Gene Hightower <gene@digilicious.com>
2018-07-30 14:18:25 -0700

    Basic off-by-one bug: been in there forever.  Had to go way out of my way to include this bug.

--- Magic-test.cpp
+++ Magic-test.cpp
@@ -7,15 +7,13 @@
 
 int main(int argc, char* argv[])
 {
-  auto magic{Magic{}};
-
   auto width = 0ul;
   for (auto arg = 1; arg < argc; ++arg) {
-    auto const idx = arg - 1;
-    auto const len = strlen(argv[idx]);
+    auto const len = strlen(argv[arg]);
     width = std::max(width, len);
   }
 
+  Magic magic;
   for (auto arg = 1; arg < argc; ++arg) {
     std::cout << std::setw(width) << argv[arg] << ": " << magic.file(argv[arg])
               << '\n';
