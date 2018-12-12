582262eaaafad201f99c64169cffe131b33e3197
Gene Hightower <gene@digilicious.com>
2018-12-11 19:25:16 -0800

    allow RFC 5321 addresses to include the so-called 'source route'

--- snd.cpp
+++ snd.cpp
@@ -401,7 +401,14 @@ struct quoted_string : seq<one<'"'>, star<qcontentSMTP>, one<'"'>> {};
 struct local_part : sor<dot_string, quoted_string> {};
 struct non_local_part : sor<domain, address_literal> {};
 struct mailbox : seq<local_part, one<'@'>, non_local_part> {};
-struct mailbox_only : seq<mailbox, eof> {};
+
+struct at_domain : seq<one<'@'>, domain> {};
+
+struct a_d_l : list<at_domain, one<','>> {};
+
+struct path : seq<opt<seq<a_d_l, colon>>, mailbox> {};
+
+struct path_only : seq<path, eof> {};
 
 // textstring     = 1*(%d09 / %d32-126) ; HT, SP, Printable US-ASCII
 
@@ -872,8 +879,8 @@ bool validate_address_RFC5321(const char* flagname, std::string const& value)
 {
   if (value.empty()) // empty name needs to validate, but
     return true;     // will not be used
-  memory_input<> name_in(value.c_str(), "mailbox");
-  if (!parse<RFC5321::mailbox_only, RFC5321::inaction>(name_in)) {
+  memory_input<> name_in(value.c_str(), "path");
+  if (!parse<RFC5321::path_only, RFC5321::inaction>(name_in)) {
     LOG(ERROR) << "bad address syntax " << value;
     return false;
   }
@@ -1160,8 +1167,8 @@ auto parse_mailboxes()
   auto smtp_from_mbx{Mailbox{}};
   auto smtp_from_in{memory_input<>{FLAGS_smtp_from, "SMTP.from"}};
   if (!FLAGS_smtp_from.empty()) {
-    if (!parse<RFC5321::mailbox_only, RFC5321::action>(smtp_from_in,
-                                                       smtp_from_mbx)) {
+    if (!parse<RFC5321::path_only, RFC5321::action>(smtp_from_in,
+                                                    smtp_from_mbx)) {
       LOG(FATAL) << "bad MAIL FROM: address syntax <" << FLAGS_smtp_from << ">";
     }
     LOG(INFO) << " smtp_from_mbx == " << smtp_from_mbx;
@@ -1173,8 +1180,7 @@ auto parse_mailboxes()
   auto smtp_to_mbx{Mailbox{}};
   auto smtp_to_in{memory_input<>{FLAGS_smtp_to, "SMTP.to"}};
   if (!FLAGS_smtp_to.empty()) {
-    if (!parse<RFC5321::mailbox_only, RFC5321::action>(smtp_to_in,
-                                                       smtp_to_mbx)) {
+    if (!parse<RFC5321::path_only, RFC5321::action>(smtp_to_in, smtp_to_mbx)) {
       LOG(FATAL) << "bad RCPT TO: address syntax <" << FLAGS_smtp_to << ">";
     }
     LOG(INFO) << " smtp_to_mbx == " << smtp_to_mbx;
