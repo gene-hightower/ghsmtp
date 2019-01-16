123802744f4310a322e5453b12f8458982318efa
Gene Hightower <gene@digilicious.com>
2019-01-15 21:58:07 -0800

    uniform init

--- CDB-test.cpp
+++ CDB-test.cpp
@@ -6,16 +6,16 @@
 
 int main(int argc, char* argv[])
 {
-  CDB no_db("no database");
+  CDB no_db{"no database"};
   CHECK(!no_db.lookup("foo"));
 
-  CDB cdb2("two-level-tlds");
+  CDB cdb2{"two-level-tlds"};
 
   CHECK(cdb2.lookup("0.bg"));
   CHECK(cdb2.lookup("zzux.com"));
   CHECK(!cdb2.lookup("This should not be found."));
 
-  CDB cdb3("three-level-tlds");
+  CDB cdb3{"three-level-tlds"};
 
   CHECK(cdb3.lookup("act.edu.au"));
   CHECK(cdb3.lookup("zen.co.uk"));
