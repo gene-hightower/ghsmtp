1c3bdac35da54cf33560eb37589af219587c1458
Gene Hightower <gene@digilicious.com>
2019-03-31 18:09:40 -0700

    make comments lie less

--- iobuffer-test.cpp
+++ iobuffer-test.cpp
@@ -6,18 +6,19 @@ int main()
 {
   iobuffer<char> v;
 
-  // fill v with values [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  // fill v with values [0..9]
   v.resize(10);
   for (size_t i = 0; i < 10; ++i)
     *(v.data() + i) = i;
 
-  // chop off the end of v, which now should be [1, 2, 3, 4, 5], but
+  // chop off the end of v, which now should be [0..4], but
   // the other 5 values should remain in memory
   v.resize(5);
 
   // grow back to 10
   v.resize(10);
 
+  // verify the values are unchanged
   for (size_t i = 0; i < v.size(); ++i) {
     CHECK_EQ(*(v.data() + i), i);
   }
