992639647922b626c535773cd5e3fc2cd7d9f758
Gene Hightower <gene@digilicious.com>
2020-02-02 13:55:48 -0800

    add 5321 rules

--- mbx.cpp
+++ mbx.cpp
@@ -41,6 +41,122 @@ struct VUCHAR : sor<VCHAR, non_ascii> {};
 // clang-format on
 } // namespace UTF8
 
+namespace RFC5321 {
+// <https://tools.ietf.org/html/rfc5321>
+// clang-format off
+
+using dot = one<'.'>;
+using colon = one<':'>;
+using dash = one<'-'>;
+
+struct u_let_dig : sor<ALPHA, DIGIT, UTF8::non_ascii> {};
+
+struct u_ldh_tail : star<sor<seq<plus<one<'-'>>, u_let_dig>, u_let_dig>> {};
+
+struct u_label : seq<u_let_dig, u_ldh_tail> {};
+
+struct let_dig : sor<ALPHA, DIGIT> {};
+
+struct ldh_tail : star<sor<seq<plus<one<'-'>>, let_dig>, let_dig>> {};
+
+struct ldh_str : seq<let_dig, ldh_tail> {};
+
+struct label : ldh_str {};
+
+struct sub_domain : sor<label, u_label> {};
+
+struct domain : list<sub_domain, dot> {};
+
+struct dec_octet : sor<seq<string<'2','5'>, range<'0','5'>>,
+                       seq<one<'2'>, range<'0','4'>, DIGIT>,
+                       seq<range<'0', '1'>, rep<2, DIGIT>>,
+                       rep_min_max<1, 2, DIGIT>> {};
+
+struct IPv4_address_literal : seq<dec_octet, dot, dec_octet, dot, dec_octet, dot, dec_octet> {};
+
+struct h16 : rep_min_max<1, 4, HEXDIG> {};
+
+struct ls32 : sor<seq<h16, colon, h16>, IPv4_address_literal> {};
+
+struct dcolon : two<':'> {};
+
+struct IPv6address : sor<seq<                                          rep<6, h16, colon>, ls32>,
+                         seq<                                  dcolon, rep<5, h16, colon>, ls32>,
+                         seq<opt<h16                        >, dcolon, rep<4, h16, colon>, ls32>,
+                         seq<opt<h16,     opt<   colon, h16>>, dcolon, rep<3, h16, colon>, ls32>,
+                         seq<opt<h16, rep_opt<2, colon, h16>>, dcolon, rep<2, h16, colon>, ls32>,
+                         seq<opt<h16, rep_opt<3, colon, h16>>, dcolon,        h16, colon,  ls32>,
+                         seq<opt<h16, rep_opt<4, colon, h16>>, dcolon,                     ls32>,
+                         seq<opt<h16, rep_opt<5, colon, h16>>, dcolon,                      h16>,
+                         seq<opt<h16, rep_opt<6, colon, h16>>, dcolon                          >> {};
+
+struct IPv6_address_literal : seq<TAOCPP_PEGTL_ISTRING("IPv6:"), IPv6address> {};
+
+struct dcontent : ranges<33, 90, 94, 126> {};
+
+struct standardized_tag : ldh_str {};
+
+struct general_address_literal : seq<standardized_tag, colon, plus<dcontent>> {};
+
+// 4.1.3.  Address Literals
+struct address_literal : seq<one<'['>,
+                             sor<IPv4_address_literal,
+                                 IPv6_address_literal,
+                                 general_address_literal>,
+                             one<']'>> {};
+
+
+struct qtextSMTP : sor<ranges<32, 33, 35, 91, 93, 126>, UTF8::non_ascii> {};
+struct graphic : range<32, 126> {};
+struct quoted_pairSMTP : seq<one<'\\'>, graphic> {};
+struct qcontentSMTP : sor<qtextSMTP, quoted_pairSMTP> {};
+
+// excluded from atext: "(),.@[]"
+struct atext : sor<ALPHA, DIGIT,
+                   one<'!', '#',
+                       '$', '%',
+                       '&', '\'',
+                       '*', '+',
+                       '-', '/',
+                       '=', '?',
+                       '^', '_',
+                       '`', '{',
+                       '|', '}',
+                       '~'>,
+                   UTF8::non_ascii> {};
+struct atom : plus<atext> {};
+struct dot_string : list<atom, dot> {};
+struct quoted_string : seq<one<'"'>, star<qcontentSMTP>, one<'"'>> {};
+struct local_part : sor<dot_string, quoted_string> {};
+struct non_local_part : sor<domain, address_literal> {};
+struct mailbox : seq<local_part, one<'@'>, non_local_part> {};
+
+// clang-format on
+// Actions
+
+template <typename Rule>
+struct action : nothing<Rule> {
+};
+
+template <>
+struct action<local_part> {
+  template <typename Input>
+  static void apply(Input const& in, Address& addr)
+  {
+    addr.local_part = in.string();
+  }
+};
+
+template <>
+struct action<non_local_part> {
+  template <typename Input>
+  static void apply(Input const& in, Address& addr)
+  {
+    addr.domain = in.string();
+  }
+};
+} // namespace RFC5321
+
 namespace RFC5322 {
 // <https://tools.ietf.org/html/rfc5322>
 // clang-format off
@@ -168,98 +284,17 @@ struct action<domain> {
 };
 } // namespace RFC5322
 
-namespace RFC5321 {
-// <https://tools.ietf.org/html/rfc5321>
-// clang-format off
+bool parse_mailbox(char const* value)
+{
+  Address addr;
 
-using dot = one<'.'>;
-using colon = one<':'>;
-using dash = one<'-'>;
+  memory_input<> address_in(value, "address");
+  if (!parse<RFC5321::mailbox, RFC5321::action>(address_in, addr)) {
+    return false;
+  }
 
-struct u_let_dig : sor<ALPHA, DIGIT, UTF8::non_ascii> {};
-
-struct u_ldh_tail : star<sor<seq<plus<one<'-'>>, u_let_dig>, u_let_dig>> {};
-
-struct u_label : seq<u_let_dig, u_ldh_tail> {};
-
-struct let_dig : sor<ALPHA, DIGIT> {};
-
-struct ldh_tail : star<sor<seq<plus<one<'-'>>, let_dig>, let_dig>> {};
-
-struct ldh_str : seq<let_dig, ldh_tail> {};
-
-struct label : ldh_str {};
-
-struct sub_domain : sor<label, u_label> {};
-
-struct domain : list<sub_domain, dot> {};
-
-struct dec_octet : sor<seq<string<'2','5'>, range<'0','5'>>,
-                       seq<one<'2'>, range<'0','4'>, DIGIT>,
-                       seq<range<'0', '1'>, rep<2, DIGIT>>,
-                       rep_min_max<1, 2, DIGIT>> {};
-
-struct IPv4_address_literal : seq<dec_octet, dot, dec_octet, dot, dec_octet, dot, dec_octet> {};
-
-struct h16 : rep_min_max<1, 4, HEXDIG> {};
-
-struct ls32 : sor<seq<h16, colon, h16>, IPv4_address_literal> {};
-
-struct dcolon : two<':'> {};
-
-struct IPv6address : sor<seq<                                          rep<6, h16, colon>, ls32>,
-                         seq<                                  dcolon, rep<5, h16, colon>, ls32>,
-                         seq<opt<h16                        >, dcolon, rep<4, h16, colon>, ls32>,
-                         seq<opt<h16,     opt<   colon, h16>>, dcolon, rep<3, h16, colon>, ls32>,
-                         seq<opt<h16, rep_opt<2, colon, h16>>, dcolon, rep<2, h16, colon>, ls32>,
-                         seq<opt<h16, rep_opt<3, colon, h16>>, dcolon,        h16, colon,  ls32>,
-                         seq<opt<h16, rep_opt<4, colon, h16>>, dcolon,                     ls32>,
-                         seq<opt<h16, rep_opt<5, colon, h16>>, dcolon,                      h16>,
-                         seq<opt<h16, rep_opt<6, colon, h16>>, dcolon                          >> {};
-
-struct IPv6_address_literal : seq<TAOCPP_PEGTL_ISTRING("IPv6:"), IPv6address> {};
-
-struct dcontent : ranges<33, 90, 94, 126> {};
-
-struct standardized_tag : ldh_str {};
-
-struct general_address_literal : seq<standardized_tag, colon, plus<dcontent>> {};
-
-// 4.1.3.  Address Literals
-struct address_literal : seq<one<'['>,
-                             sor<IPv4_address_literal,
-                                 IPv6_address_literal,
-                                 general_address_literal>,
-                             one<']'>> {};
-
-
-struct qtextSMTP : sor<ranges<32, 33, 35, 91, 93, 126>, UTF8::non_ascii> {};
-struct graphic : range<32, 126> {};
-struct quoted_pairSMTP : seq<one<'\\'>, graphic> {};
-struct qcontentSMTP : sor<qtextSMTP, quoted_pairSMTP> {};
-
-// excluded from atext: "(),.@[]"
-struct atext : sor<ALPHA, DIGIT,
-                   one<'!', '#',
-                       '$', '%',
-                       '&', '\'',
-                       '*', '+',
-                       '-', '/',
-                       '=', '?',
-                       '^', '_',
-                       '`', '{',
-                       '|', '}',
-                       '~'>,
-                   UTF8::non_ascii> {};
-struct atom : plus<atext> {};
-struct dot_string : list<atom, dot> {};
-struct quoted_string : seq<one<'"'>, star<qcontentSMTP>, one<'"'>> {};
-struct local_part : sor<dot_string, quoted_string> {};
-struct non_local_part : sor<domain, address_literal> {};
-struct mailbox : seq<local_part, one<'@'>, non_local_part> {};
-
-// clang-format on
-} // namespace RFC5321
+  return true;
+}
 
 bool parse_address(char const* value)
 {
@@ -278,4 +313,9 @@ int main()
   assert(!parse_address("foo bar@digilicious.com"));
   assert(parse_address("gene@digilicious.com"));
   assert(parse_address("Gene Hightower <gene@digilicious.com>"));
+  assert(parse_address("gene@[127.999.0.1]"));
+
+  assert(parse_mailbox("gene@digilicious.com"));
+  assert(parse_mailbox("gene@[127.0.0.1]"));
+  assert(!parse_mailbox("gene@[127.999.0.1]"));
 }
