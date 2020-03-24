d2ef87488235877113453d23f6344bb2936c4edc
Gene Hightower <gene@digilicious.com>
2020-03-23 19:05:42 -0700

    check for IPv4 wrapped up like v6

--- Sock.cpp
+++ Sock.cpp
@@ -5,6 +5,24 @@
 
 #include <glog/logging.h>
 
+static bool is_ipv4_mapped_ipv6_addresses(in6_addr const& sa)
+{
+  // clang-format off
+  return sa.s6_addr[0] == 0 &&
+         sa.s6_addr[1] == 0 &&
+         sa.s6_addr[2] == 0 &&
+         sa.s6_addr[3] == 0 &&
+         sa.s6_addr[4] == 0 &&
+         sa.s6_addr[5] == 0 &&
+         sa.s6_addr[6] == 0 &&
+         sa.s6_addr[7] == 0 &&
+         sa.s6_addr[8] == 0 &&
+         sa.s6_addr[9] == 0 &&
+         sa.s6_addr[10] == 0xff &&
+         sa.s6_addr[11] == 0xff;
+  // clang-format on
+}
+
 Sock::Sock(int                       fd_in,
            int                       fd_out,
            std::function<void(void)> read_hook,
@@ -12,7 +30,7 @@ Sock::Sock(int                       fd_in,
            std::chrono::milliseconds write_timeout,
            std::chrono::milliseconds starttls_timeout)
   : iostream_(
-        fd_in, fd_out, read_hook, read_timeout, write_timeout, starttls_timeout)
+      fd_in, fd_out, read_hook, read_timeout, write_timeout, starttls_timeout)
 {
   // Get our local IP address as "us".
 
@@ -56,10 +74,19 @@ Sock::Sock(int                       fd_in,
       break;
 
     case sizeof(sockaddr_in6):
-      PCHECK(inet_ntop(AF_INET6, &them_addr_.addr_in6.sin6_addr, them_addr_str_,
-                       sizeof them_addr_str_)
-             != nullptr);
-      them_address_literal_ = IP6::to_address_literal(them_addr_str_);
+      if (is_ipv4_mapped_ipv6_addresses(them_addr_.addr_in6.sin6_addr)) {
+        PCHECK(inet_ntop(AF_INET, &them_addr_.addr_in6.sin6_addr.s6_addr[12],
+                         them_addr_str_, sizeof them_addr_str_)
+               != nullptr);
+        them_address_literal_ = IP4::to_address_literal(them_addr_str_);
+        LOG(INFO) << "IPv4 disguised as IPv6: " << them_addr_str_;
+      }
+      else {
+        PCHECK(inet_ntop(AF_INET6, &them_addr_.addr_in6.sin6_addr,
+                         them_addr_str_, sizeof them_addr_str_)
+               != nullptr);
+        them_address_literal_ = IP6::to_address_literal(them_addr_str_);
+      }
       break;
 
     default:
