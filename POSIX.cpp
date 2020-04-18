88297c92e6c40749fcdeed64131042dc7b04a212
Gene Hightower <gene@digilicious.com>
2020-04-17 17:20:32 -0700

    another crack at POSIX read and write

--- POSIX.cpp
+++ POSIX.cpp
@@ -52,90 +52,92 @@ bool POSIX::output_ready(int fd_out, milliseconds wait)
   return 0 != puts;
 }
 
-std::streamsize POSIX::read_fd_(time_point<system_clock>  start,
-                                std::function<void(void)> read_hook,
-                                int                       fd,
-                                char*                     s,
-                                std::streamsize           n,
-                                milliseconds              timeout,
-                                bool&                     t_o)
+std::streamsize POSIX::read(int                       fd,
+                            char*                     s,
+                            std::streamsize           n,
+                            std::function<void(void)> read_hook,
+                            std::chrono::milliseconds timeout,
+                            bool&                     t_o)
 {
-  auto end_time = start + timeout;
+  auto const start    = std::chrono::system_clock::now();
+  auto const end_time = start + timeout;
 
-  ssize_t n_ret;
-  while ((n_ret = ::read(fd, static_cast<void*>(s), static_cast<size_t>(n)))
-         < 0) {
+  for (;;) {
+    auto const n_ret = ::read(fd, static_cast<void*>(s), n);
 
-    if (errno == EINTR)
-      continue; // try read again
+    if (n_ret == -1) {
+      switch (errno) {
+      case EINTR: break; // try read again
 
-    if (errno == ECONNRESET) {
-      // LOG(WARNING) << "read(2) raised ECONNRESET, interpreting as EOF";
-      return static_cast<std::streamsize>(-1);
+      case ECONNRESET:
+        LOG(WARNING) << "read(2) raised ECONNRESET";
+        return -1;
+
+      default:
+        PCHECK((errno == EWOULDBLOCK) || (errno == EAGAIN))
+            << "error from read(2)";
+      }
+    }
+    else if (n_ret >= 0) {
+      return n_ret;
     }
 
-    PCHECK((errno == EWOULDBLOCK) || (errno == EAGAIN)) << "error from read(2)";
-
-    auto now = system_clock::now();
+    auto const now = system_clock::now();
     if (now < end_time) {
-      auto time_left = duration_cast<milliseconds>(end_time - now);
+      auto const time_left = duration_cast<milliseconds>(end_time - now);
       read_hook();
       if (input_ready(fd, time_left))
         continue; // try read again
     }
     t_o = true;
     LOG(WARNING) << "read(2) timed out";
-    return static_cast<std::streamsize>(-1);
+    return -1;
   }
-
-  return n_ret;
 }
 
-std::streamsize POSIX::write_fd_(time_point<system_clock> start,
-                                 int                      fd,
-                                 char const*              s,
-                                 std::streamsize          n,
-                                 milliseconds             timeout,
-                                 bool&                    t_o)
+std::streamsize POSIX::write(int                       fd,
+                             const char*               s,
+                             std::streamsize           n,
+                             std::chrono::milliseconds timeout,
+                             bool&                     t_o)
 {
-  auto end_time = start + timeout;
+  auto const start    = std::chrono::system_clock::now();
+  auto const end_time = start + timeout;
 
-  ssize_t n_ret;
-  while (
-      (n_ret = ::write(fd, static_cast<const void*>(s), static_cast<size_t>(n)))
-      < 0) {
+  auto written = std::streamsize{};
 
-    if (errno == EINTR)
-      continue; // try write again
+  for (;;) {
+    auto const n_ret = ::write(fd, static_cast<const void*>(s), n - written);
 
-    if (errno == ECONNRESET) {
-      // LOG(WARNING) << "write(2) raised ECONNRESET, interpreting as EOF";
-      return static_cast<std::streamsize>(-1);
+    if (n_ret == -1) {
+      switch (errno) {
+      case EINTR: break; // try write again
+
+      case ECONNRESET:
+        LOG(WARNING) << "write(2) raised ECONNRESET";
+        return -1;
+
+      default:
+        PCHECK((errno == EWOULDBLOCK) || (errno == EAGAIN))
+            << "error from write(2)";
+      }
+    }
+    else {
+      s += n_ret;
+      written += n_ret;
     }
 
-    PCHECK((errno == EWOULDBLOCK) || (errno == EAGAIN))
-        << "error from write(2)";
+    if (written == n)
+      return n;
 
-    auto now = system_clock::now();
+    auto const now = system_clock::now();
     if (now < end_time) {
-      auto time_left = duration_cast<milliseconds>(end_time - now);
+      auto const time_left = duration_cast<milliseconds>(end_time - now);
       if (output_ready(fd, time_left))
-        continue; // try write again
+        continue; // write some more
     }
     t_o = true;
     LOG(WARNING) << "write(2) time out";
-    return static_cast<std::streamsize>(-1);
+    return -1;
   }
-
-  // The stream buffer code above us can deal with a short read, but
-  // not a short write.
-
-  while (n_ret && (n_ret < n)) {
-    auto next_n = write_fd_(start, fd, s + n_ret, n - n_ret, timeout, t_o);
-    if (next_n == -1)
-      break;
-    n_ret += next_n;
-  }
-
-  return n_ret;
 }
--- POSIX.hpp
+++ POSIX.hpp
@@ -19,44 +19,18 @@ public:
   static bool input_ready(int fd_in, std::chrono::milliseconds wait);
   static bool output_ready(int fd_out, std::chrono::milliseconds wait);
 
-  static std::streamsize read(int                       fd_in,
+  static std::streamsize read(int                       fd,
                               char*                     s,
                               std::streamsize           n,
                               std::function<void(void)> read_hook,
                               std::chrono::milliseconds timeout,
-                              bool&                     t_o)
-  {
-    return read_fd_(std::chrono::system_clock::now(), read_hook, fd_in, s, n,
-                    timeout, t_o);
-  }
+                              bool&                     t_o);
 
-  static std::streamsize write(int                       fd_out,
+  static std::streamsize write(int                       fd,
                                const char*               s,
                                std::streamsize           n,
                                std::chrono::milliseconds timeout,
-                               bool&                     t_o)
-  {
-    return write_fd_(std::chrono::system_clock::now(), fd_out, s, n, timeout,
-                     t_o);
-  }
-
-private:
-  static std::streamsize
-  read_fd_(std::chrono::time_point<std::chrono::system_clock> start,
-           std::function<void(void)>                          read_hook,
-           int                                                fd,
-           char*                                              s,
-           std::streamsize                                    n,
-           std::chrono::milliseconds                          timeout,
-           bool&                                              t_o);
-
-  static std::streamsize
-  write_fd_(std::chrono::time_point<std::chrono::system_clock> start,
-            int                                                fd,
-            char const*                                        s,
-            std::streamsize                                    n,
-            std::chrono::milliseconds                          timeout,
-            bool&                                              t_o);
+                               bool&                     t_o);
 };
 
 #endif // POSIX_DOT_HPP
