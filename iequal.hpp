aec8d78263d42370693f6eaae1ca0b5f5f384e65
Gene Hightower <gene@digilicious.com>
2020-09-04 22:23:28 -0700

    a few more things

--- iequal.hpp
+++ iequal.hpp
@@ -1,6 +1,7 @@
 #ifndef IEQUAL_DOT_HPP
 #define IEQUAL_DOT_HPP
 
+#include <algorithm>
 #include <cctype>
 #include <string_view>
 
@@ -17,6 +18,27 @@ inline bool iequal(std::string_view a, std::string_view b)
   return (size(a) == size(b)) &&
          std::equal(begin(b), end(b), begin(a), iequal_char);
 }
+
+inline bool iless_char(char a, char b)
+{
+  return std::toupper(static_cast<unsigned char>(a)) <
+         std::toupper(static_cast<unsigned char>(b));
+}
+
+inline bool iless(std::string_view a, std::string_view b)
+{
+  return std::lexicographical_compare(begin(a), end(a), begin(b), end(b),
+                                      iless_char);
+}
+
+struct ci_less {
+  bool operator()(std::string_view a, std::string_view b) const
+  {
+    return iless(a, b);
+  }
+};
+
+// Ahh, the olden days of loops and pointers...
 
 // inline bool iequal(char const* a, char const* b)
 // {
