02bfc60c2e06205f639669c8355ac1fac9d19020
Gene Hightower <gene@digilicious.com>
2020-12-19 10:03:50 -0800

    more generic

--- is_ascii.hpp
+++ is_ascii.hpp
@@ -1,14 +1,13 @@
 #include <algorithm>
 #include <string_view>
 
-constexpr bool isascii(char c) noexcept
+constexpr bool isascii(auto c) noexcept
 {
   return (static_cast<unsigned char>(c) & 0x80) == 0;
 }
 
 constexpr bool is_ascii(std::string_view str) noexcept
 {
-  return !std::any_of(std::begin(str), std::end(str), [](char ch) {
-    return !isascii(static_cast<unsigned char>(ch));
-  });
+  return !std::any_of(std::begin(str), std::end(str),
+                      [](auto ch) { return !isascii(ch); });
 }
