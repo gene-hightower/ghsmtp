dbf6df1e703e0bfb23a45a85dd29282cb4575b42
Gene Hightower <gene@digilicious.com>
2022-04-07 08:29:34 -0700

    strcmp does not return bool

--- TLD-test.cpp
+++ TLD-test.cpp
@@ -12,16 +12,19 @@ int main(int argc, char const* argv[])
 
   CHECK_NOTNULL(tld.get_registered_domain("foo.blogspot.com.ar"));
 
-  CHECK(!strcmp(tld.get_registered_domain("pi.digilicious.com"),
-                "digilicious.com"));
+  CHECK_EQ(strcmp(tld.get_registered_domain("pi.digilicious.com"),
+                  "digilicious.com"),
+           0);
 
-  CHECK(!strcmp(tld.get_registered_domain("outmail14.phi.meetup.com"),
-                "meetup.com"));
+  CHECK_EQ(strcmp(tld.get_registered_domain("outmail14.phi.meetup.com"),
+                  "meetup.com"),
+           0);
 
   CHECK(nullptr == tld.get_registered_domain("not_a_domain_at_all"));
   CHECK(nullptr == tld.get_registered_domain(".com"));
   CHECK(nullptr == tld.get_registered_domain("."));
 
-  CHECK(!strcmp(tld.get_registered_domain("reward.yournewestbonuspoints.com"),
-                "yournewestbonuspoints.com"));
+  CHECK_EQ(strcmp(tld.get_registered_domain("reward.yournewestbonuspoints.com"),
+                  "yournewestbonuspoints.com"),
+           0);
 }
