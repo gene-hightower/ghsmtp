2a3f7b1f0f83c5e738a98f96ce10c16fe1e6568a
Gene Hightower <gene@digilicious.com>
2024-05-12 10:14:30 -0700

    not used

--- pcg.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef PCG_DOT_HPP_INCLUDED
-#define PCG_DOT_HPP_INCLUDED
-
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wold-style-cast"
-#include "pcg_random.hpp"
-#pragma GCC diagnostic pop
-
-#endif // PCG_DOT_HPP_INCLUDED
