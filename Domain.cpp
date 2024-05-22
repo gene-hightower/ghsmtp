6eb046e664eae5852226e5687f5a50956ed75e47
Gene Hightower <gene@digilicious.com>
2024-05-22 08:49:41 -0700

    syntax chack our dot-string

--- Domain.cpp
+++ Domain.cpp
@@ -11,11 +11,82 @@
 
 #include <glog/logging.h>
 
+#include <tao/pegtl.hpp>
+#include <tao/pegtl/contrib/abnf.hpp>
+
+using namespace tao::pegtl;
+using namespace tao::pegtl::abnf;
+
 #include <stdexcept>
+
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/split.hpp>
+
+namespace RFC5321 {
+#include "UTF8.hpp"
+
+using dot   = one<'.'>;
+using dash  = one<'-'>;
+
+struct u_let_dig : sor<ALPHA, DIGIT, UTF8_non_ascii> {};
+
+struct u_ldh_tail : star<sor<seq<plus<dash>, u_let_dig>, u_let_dig>> {};
+
+struct u_label : seq<u_let_dig, u_ldh_tail> {};
+
+struct let_dig : sor<ALPHA, DIGIT> {};
+
+struct ldh_tail : star<sor<seq<plus<dash>, let_dig>, let_dig>> {};
+
+struct ldh_str : seq<let_dig, ldh_tail> {};
+
+struct sub_domain : u_label {};
+
+struct domain : list_tail<sub_domain, dot> {};
+};
 
 namespace {
 size_t constexpr max_length = 255;
+
+bool is_domain(std::string_view dom)
+{
+  auto in{memory_input<>(dom.data(), dom.size(), "domain")};
+  return tao::pegtl::parse<RFC5321::domain>(in);
 }
+
+bool domain_check(std::string_view dom)
+{
+  if (dom.empty()) {
+    return true; // domains in email addresses can be empty
+  }
+
+  if (!is_domain(dom)) {
+    LOG(ERROR) << "failed to parse «" << dom << "» as domain";
+    return false;
+  }
+
+  /*
+   * Allow "localhost" amung others.
+
+  std::string domain(dom.data(), dom.length());
+
+  auto labels{std::vector<std::string>{}};
+  boost::algorithm::split(labels, domain, boost::algorithm::is_any_of("."));
+
+  if (labels.size() < 2) {
+    LOG(ERROR) << "domain «" << dom << "» must have two or more labels";
+    return false;
+  }
+
+  if (labels[labels.size() - 1].length() < 2) {
+    LOG(ERROR) << "TLD must be two or more chars in «" << dom << "»";
+    return false;
+  }
+  */
+
+  return true;
+}
+} // namespace
 
 // Normalization Form KC (NFKC) Compatibility Decomposition, followed
 // by Canonical Composition, see <http://unicode.org/reports/tr15/>
@@ -65,7 +136,9 @@ bool Domain::validate(std::string_view dom)
     return false;
   idn2_free(ptr);
 
-  // FIXME: check syntax is dot-string?
+  if (!domain_check(ascii)) {
+    return false;
+  }
 
   return true;
 }
@@ -116,4 +189,8 @@ void Domain::set(std::string_view dom)
     throw std::invalid_argument(idn2_strerror(code));
   utf8_ = ptr;
   idn2_free(ptr);
+
+  if (!domain_check(ascii_)) {
+    throw std::invalid_argument("domain not correct");
+  }
 }
